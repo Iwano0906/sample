b9c21b8a414be2daab6634d618104e671c71ac2b
Iwano0906 <Ryoga.0906.Mail@gmail.com>
2024-07-10 10:00:54 +0900

    new commit

---
+++ assets/js/textChange.js
@@ -0,0 +1,55 @@
+
+// card-text
+let text1 = document.getElementById('card-text1').textContent;
+
+document.getElementById('card-text1').textContent 
+// = '';
+
+let text2 = document.getElementById('card-text2').textContent;
+
+document.getElementById('card-text2').textContent 
+// = '';
+
+let text3 = document.getElementById('card-text3').textContent;
+
+document.getElementById('card-text3').textContent 
+// = '';
+
+
+// card-descText
+let descriptionText1 = document.getElementById('card-descText1').textContent;
+
+document.getElementById('card-descText1').textContent 
+// = ''
+
+let descriptionText2 = document.getElementById('card-descText2').textContent;
+
+document.getElementById('card-descText2').textContent 
+// = ''
+
+let descriptionText3 = document.getElementById('card-descText3').textContent;
+
+document.getElementById('card-descText3').textContent 
+// = ''
+
+
+
+
+// time
+let nowTime1 = document.getElementById('time-text1').textContent;
+
+document.getElementById('time-text1').textContent 
+// = '2023-08-10';
+= '2023-08-11';
+
+let nowTime2 = document.getElementById('time-text2').textContent;
+
+document.getElementById('time-text2').textContent 
+// = '2023-08-10';
+= '2023-08-11';
+
+let nowTime3 = document.getElementById('time-text3').textContent;
+
+document.getElementById('time-text3').textContent 
+// = '2023-08-10';
+= '2023-08-11';
